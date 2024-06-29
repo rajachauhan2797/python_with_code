509aa635fb9ab1bddb3e589e952bf710e74669d4
rajaCShipyaari <raja.chauhan@shipyaari.com>
2024-06-29 17:08:21 +0530

    import and export used

---
+++ 1.basics/chai.py
@@ -0,0 +1,5 @@
+from hello_chai import chai, chai_one, chai_two
+
+chai("5")
+
+print("Hello World",chai_two)
---
+++ 1.basics/hello_chai.py
@@ -0,0 +1,9 @@
+
+def chai(n):
+    print("Hello Chai", n)
+
+
+chai_one = "chai_one"
+chai_two = "chai_two"
+chai_three = "chai_three"
+chai_four = "chai_four"
